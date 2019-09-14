30318e401228d3477d0a8c9e13b5a5d0b2c43298
Alexandra Dima <adadima@mit.edu>
2019-09-14 15:53:47 -0400

    Started implementing activity plotting function.

--- activity_tracker.py
+++ activity_tracker.py
@@ -1,5 +1,31 @@
 def zoes_func(log_file, timestamp_start=None, timestamp_end=None):
-    pass
+    yield [{"2": " "}, {"1": " "}]
 
-def plot(log_file):
-    pass
+
+# date, time, tag, source, text
+def plot(log_file, start, end):
+    time_since_last_commit = 0
+    for line in zoes_func(log_file, start, end):
+        time_since_last_commit += 0
+        if "COMPILATION STARTED" in line["text"]:
+            pass
+        if "COMPILATION FINISHED" in line["text"]:
+            pass
+        if "IDE STARTED" in line["text"]:
+            pass
+        if "IDE SHUTDOWN" in line["text"]:
+            pass
+        if "Saving Project" in line["text"]:
+            pass
+        if "ERROR" in line["tag"]:
+            pass
+        if "commit" in line["text"] and "GitHandler" in line["source"]:
+            pass
+
+
+def plot_point(data_generator, start, end):
+    time = start
+    num_events = 0
+    while time < start + 1 and time < end:
+        for line in data_generator:
+            pass
--- events.py
+++ events.py
@@ -46,6 +46,66 @@ class TimeStamp:
 
 
 class Event:
-    def __init__(self, tag: EventTag, time_stamp: TimeStamp):
+    def __init__(self, tag: EventTag, time_stamp: TimeStamp, graph_color):
         self.tag = tag
         self.time = time_stamp
+        self.graph_color = graph_color
+
+    def get_year(self):
+        return self.time.date.year
+
+    def get_month(self):
+        return self.time.date.month
+
+    def get_day(self):
+        return self.time.date.day
+
+    def get_hour(self):
+        return self.time.time.hour
+
+    def get_minute(self):
+        return self.time.time.minute
+
+    def get_second(self):
+        return self.time.time.second
+
+
+class IDEStartedEvent(Event):
+    def __init__(self, time_stamp: TimeStamp, graph_color):
+        super(IDEStartedEvent, self).__init__(InfoTag, time_stamp, graph_color)
+
+
+class IDEShutdownEvent(Event):
+    def __init__(self, time_stamp: TimeStamp, graph_color):
+        super(IDEShutdownEvent, self).__init__(InfoTag, time_stamp, graph_color)
+
+
+class CompilationStartedEvent(Event):
+    def __init__(self, time_stamp: TimeStamp, graph_color):
+        super(CompilationStartedEvent, self).__init__(InfoTag, time_stamp, graph_color)
+
+
+class CompilationFinishedEvent(Event):
+    def __init__(self, time_stamp: TimeStamp, duration: int, errors: int, warnings: int, graph_color):
+        super(CompilationFinishedEvent, self).__init__(InfoTag, time_stamp, graph_color)
+        self.duration = duration
+        self.errors = errors
+        self.warnings = warnings
+
+
+class SaveEvent(Event):
+    def __init__(self, time_stamp: TimeStamp, duration: int, graph_color):
+        super(SaveEvent, self).__init__(InfoTag, time_stamp, graph_color)
+        self.duration = duration
+
+
+class ErrorEvent(Event):
+    def __init__(self, time_stamp: TimeStamp, text: str, graph_color):
+        super(ErrorEvent, self).__init__(ErrorTag, time_stamp, graph_color)
+        self.text = text
+
+
+class GitCommitEvent(Event):
+    def __init__(self, time_stamp: TimeStamp, graph_color, time_since_last=None):
+        super(GitCommitEvent, self).__init__(InfoTag, time_stamp, graph_color)
+        self.duration = time_since_last
